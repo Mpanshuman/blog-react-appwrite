fea7ccc20ee8219793d37bc76027227f152e4792
Mpanshuman <m.p.ansuman10@gmail.com>
2025-03-13 23:20:43 +0530

    checked user logged in or not

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,41 @@
+import { useEffect, useState } from "react";
 import "./App.css";
-import conf from "./conf/conf";
+// import conf from "./conf/conf";
+import { Header, Footer } from "./components";
+import { useDispatch } from "react-redux";
+import { login, logout } from "./store/authSlice";
+import authService from "./appwrite/auth";
 function App() {
-  console.log(conf.appwriteUrl);
-  return (
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+        setLoading(false);
+      })
+      .catch((error) =>
+        console.log("Appwrite service :: getCurrentUser :: Mainerror:", error)
+      )
+      .finally(() => setLoading(false));
+  }, []);
+  return !loading ? (
     <>
-      <h1>Hello</h1>
+      <div className=" min-h-screen flex flex-wrap content-between bg-gray-400">
+        <div className=" w-full block">
+          <Header />
+          <Footer />
+        </div>
+      </div>
+    </>
+  ) : (
+    <>
+      <h1>Loading</h1>
     </>
   );
 }
