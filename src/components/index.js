ab0fc97bde4f16e867e40ff7f2ba43df9a2e5684
Mpanshuman <m.p.ansuman10@gmail.com>
2025-03-15 00:27:01 +0530

    added pages and components

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,28 @@
 import Footer from "./Footer/Footer";
 import Header from "./Header/Header";
-export { Footer, Header };
+import Logo from "./Logo";
+import Container from "./container/Container";
+import LogoutBtn from "./Header/LogoutBtn";
+import Input from "./input";
+import Button from "./button";
+import Select from "./Select";
+import RTE from "./RTE";
+import PostForm from "./post-form/PostForm";
+import Signup from "./Signup";
+import Login from "./Login";
+import PostCard from "./PostCard";
+export {
+  Footer,
+  Header,
+  Logo,
+  LogoutBtn,
+  Container,
+  Input,
+  Button,
+  Select,
+  RTE,
+  PostForm,
+  Signup,
+  Login,
+  PostCard,
+};
